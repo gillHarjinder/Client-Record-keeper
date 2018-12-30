4674571b63a646467636bc708550b3511f074dc8
gillHarjinder <laddivaliant.0001@gmail.com>
2018-12-30 12:00:05 -0800

    make changes in gitignore file

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -4,7 +4,8 @@ import spinner from './spinner.gif';
 export default () => {
   return (
     <div>
-          <img src={spinner} alt="Loading..." style={{ width: '200px', margin: 'auto', display: 'block' }} />
+          <img src={spinner} alt="Loading..." 
+          style={{ width: '200px', margin: 'auto', display: 'block' }} />
     </div>
   )
 }
